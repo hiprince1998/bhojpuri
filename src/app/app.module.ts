cc525ab7509fa379fea51da1e87df4ed900ccc4f
@princekumar <princekumarcena@gmail.com>
2017-12-26 00:14:34 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,30 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { YoutubeVideoPlayer } from '@ionic-native/youtube-video-player';
+import { YoutubePipe } from '../pipes/youtube/youtube';
+import{HttpModule}from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { PlaylistPage } from '../pages/playlist/playlist';
+ 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp,HomePage,PlaylistPage,YoutubePipe
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp,HomePage,PlaylistPage
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen,YoutubeVideoPlayer,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
+ import { PlaylistPage } from '../playlist/playlist';
 
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  channelId:string='UC22nIfOTM7KLIQuFGMKzQbg'
+  ;maxRes:string='50';
+  googleToken:string='AIzaSyCPgbtZ_2iIoavN2VZJd52ro7C6IhN8y8Y';
+ search:String='';
+  post:any=[];
+    constructor(public navCtrl: NavController,private http:Http) {
+  
+    }
 
-  constructor(public navCtrl: NavController) {
+    ionViewDidLoad(){
+      let url=" https://www.googleapis.com/youtube/v3/playlists?part=id,snippet&channelId="+this.channelId+"&q="+this.search+"&type=video&order=date&maxResults="+this.maxRes+"&key="+this.googleToken;
+      this.http.get(url).map(res=>res.json()).subscribe(data=>{
+        this.post=this.post.concat(data.items)
+        console.log(this.post);
+      })
+    }
 
-  }
-
+     pl(id){
+   this.navCtrl.push(PlaylistPage,{'item':id})
+      }
+    
 }
